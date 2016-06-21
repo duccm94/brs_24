70964d02f13dfc03f5b5ab0155953003106ba3c3
duccm94 <minhduc.chu.94@gmail.com>
2016-06-21 09:18:23 +0700

    Admin update user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,39 @@
 class Admin::UsersController < ApplicationController
   before_action :require_admin
+  before_action :find_user
 
   def index
     @users = User.paginate page: params[:page]
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t :userupdated
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
+  end
+
   def destroy
-    User.find_by_id(params[:id]).destroy
+    @user.destroy
     flash[:success] = t :destroysuccess
     redirect_to admin_users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email
+  end
+
+  def find_user
+    @user = User.find_by_id params[:id]
+    if @user.nil?
+      flash[:danger] = t :findfail
+      redirect_to admin_users_path
+    end
   end
 end
