140bb8a8187af1059ac77603aa61043c4d705ff3
duccm94 <minhduc.chu.94@gmail.com>
2016-06-21 15:08:41 +0700

    Admin create show category

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,18 @@ User.create! name: "test", email: "test@gmail.com", password: "123456",
 end
 
 50.times do |n|
-  title  = Faker::Name.name
+  title = Faker::Book.title
   publish_date = "publish_date"
   rating = 7
-  author = "author"
+  author = Faker::Book.author
   number_pages = 100
   category_id = 1
-  Book.create! title:  title, category_id: category_id, publish_date: publish_date,
-    rating: rating, author: author, number_pages: number_pages
+  Book.create! title: title, category_id: category_id,
+    publish_date: publish_date, rating: rating, author: author,
+    number_pages: number_pages
 end
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+20.times do |n|
+  name = Faker::Book.genre
+  Category.create! name: name
+end
