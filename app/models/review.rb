c225c4c2245323b19bd21850b2d35dd60dd07a0b
VuThiHien94 <khoanglang2368@gmail.com>
2016-06-21 10:12:04 +0700

    add review book

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,8 @@ class Review < ActiveRecord::Base
   belongs_to :book
   
   has_many :comments, dependent: :destroy
+
+  validates :book_id, presence: true
+  validates :user_id, presence: true
+  validates :content, presence: true, length: {maximum: 255}
 end
