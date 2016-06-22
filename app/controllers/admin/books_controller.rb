dd33a19dd0282ccd7ffd6bc026a4e1ff1fca0c84
duccm94 <minhduc.chu.94@gmail.com>
2016-06-22 15:06:31 +0700

    Admin update delete book

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,5 +1,6 @@
 class Admin::BooksController < ApplicationController
   before_action :require_admin
+  before_action :find_book, only: [:edit, :update, :destroy]
 
   def index
     @books = Book.paginate page: params[:page]
@@ -19,9 +20,36 @@ class Admin::BooksController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @book.update_attributes book_params
+      flash[:success] = t :bookupdated
+      redirect_to admin_books_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book.destroy
+    flash[:success] = t :destroysuccess
+    redirect_to admin_books_path
+  end
+
   private
   def book_params
     params.require(:book).permit :title, :publish_date, :author,
       :number_pages, :picture, :category_id
+  end
+
+  def find_book
+    @categories = Category.all
+    @book = Book.find_by_id params[:id]
+    if @book.nil?
+      flash[:danger] = t :findfail
+      redirect_to admin_books_path
+    end
   end
 end
